a649485ef147cae34768cf96a024aaed7cde8f41
unknown <juandiego14012003@gmail.com>
2023-01-25 20:02:10 -0500

    Fixing get data

--- routers/editors.routes.js
+++ routers/editors.routes.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
     const cProyecto = require('../controllers/proyectoController.js');
     const cPublicacion = require('../controllers/publicacionController.js');
     const cTrato = require('../controllers/tratoController.js');
-    const cOpinion = require('../controllers/opinionController.js');    
+    const cOpinion = require('../controllers/opinionController.js');
 
     // Rutas principales para proyecto
     app.route('/proyecto')
@@ -29,7 +29,7 @@ module.exports = function (app) {
 
         // Actualizar el proyecto de un editor
         .put((req, res) => {
-            cProyecto.updateId({_id: req.body._id}, req.body)
+            cProyecto.updateId({ _id: req.body._id }, req.body)
                 .then(data => res.send(data))
                 .catch(err => res.send(err));
         })
@@ -40,10 +40,10 @@ module.exports = function (app) {
                 .then(data => res.send(data))
                 .catch(err => res.send(err));
         });
-    
+
     // Rutas secundarias para proyecto
     app.route('/proyecto-verificar-id')
-        
+
         // Verificar existencia de proyecto por id
         .get((req, res) => {
             cProyecto.verifyId(req.query)
@@ -70,7 +70,7 @@ module.exports = function (app) {
 
         // Actualizar una publicación
         .put((req, res) => {
-            cPublicacion.updateId({_id: req.body._id}, req.body)
+            cPublicacion.updateId({ _id: req.body._id }, req.body)
                 .then(data => res.send(data))
                 .catch(err => res.send(err));
         })
@@ -85,11 +85,11 @@ module.exports = function (app) {
     // Rutas secundarias para publicación
     app.route('/publicacion-verificar-id')
         .get((req, res) => {
-            cPublicacion.searchId({_id: req.query._id})
+            cPublicacion.searchId({ _id: req.query._id })
                 .then(data => res.send(data))
                 .catch(err => res.send(err));
         })
-    
+
     // Rutas principales para trato
     app.route('/trato')
 
@@ -105,7 +105,7 @@ module.exports = function (app) {
             cTrato.createNew(req.body)
                 .then(data => res.send(data))
                 .catch(err => res.send(err));
-                
+
         })
 
         // Actualizar un trato 
@@ -125,7 +125,7 @@ module.exports = function (app) {
                 .then(data => res.send(data))
                 .catch(err => res.send(err));
         });
-    
+
     // Rutas secundarias para trato
     app.route('/trato-verificar-id')
 
@@ -140,7 +140,7 @@ module.exports = function (app) {
     app.route('/usuario')
 
         // Para registrarse
-        .post((req, res) => {            
+        .post((req, res) => {
             cUsuario.createNew(req.body)
                 .then(data => res.send(data))
                 .catch(err => res.send(err));
@@ -148,14 +148,14 @@ module.exports = function (app) {
 
         // Para actualizar el usuario
         .put((req, res) => {
-            cUsuario.updateNick({nick: req.body.nick}, req.body)
+            cUsuario.updateNick({ nick: req.body.nick }, req.body)
                 .then(data => res.send(data))
                 .catch(err => res.send(err));
         })
 
         // Para eliminar un usuario
         .delete((req, res) => {
-            cUsuario.deleteNick({nick: req.body.nick})
+            cUsuario.deleteNick({ nick: req.body.nick })
                 .then(data => res.send(data))
                 .catch(err => res.send(err));
         });
@@ -167,19 +167,40 @@ module.exports = function (app) {
                 .then(data => res.send(data))
                 .catch(err => res.send(err));
         })
-    
+
     // Rutas secundarias para usuario
+    app.route('/usuario/datos')
+
+        .get((req, res) => {
+
+            cUsuario.nickUser(req.query)
+                .then(response => {
+                    res.json({
+                        "nombre": response["nombre"],
+                        "apellido": response["apellido"],
+                        "biografia": response["biografia"]
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.json({
+                        "code": 500,
+                        "message": "Ocurrió un error mientras se intentó consultar el usuario mediante el nick en la base de datos."
+                    })
+                })
+        })
+
     app.route('/usuario-verificar-nick')
-        
+
         // Verificar si ya existe o no un nickname
         .get((req, res) => {
             cUsuario.nickUser(req.query)
                 .then(data => res.send(data))
                 .catch(err => res.send(err));
         })
-    
+
     app.route('/usuario-listar')
-        
+
         // Listar todos los usuarios
         .get((req, res) => {
             cUsuario.list()
@@ -187,123 +208,123 @@ module.exports = function (app) {
                 .catch(err => res.send(err));
         })
 
-    
+
     // Rutas para videos por _id
     app.route('/videos/:_id')
 
         // Obtener video por _id
-        .get((req, res) => {            
+        .get((req, res) => {
             mongodb.MongoClient.connect(uri, function (error, client) {
                 if (error) {
-                  res.status(500).json(error);
-                  return;
-                }        
-            
+                    res.status(500).json(error);
+                    return;
+                }
+
                 const db = client.db('Editors');
 
                 // GridFS Collection
                 db.collection('fs.files').findOne({}, (err, video) => {
-                  
+
                     if (!video) {
-                    res.status(404).send("No video uploaded!");
-                    return;
-                  }
-            
-                  // Create response headers
-                  const videoSize = video.length;
-                  const start = 0;
-                  console.log("START:");
-                  console.log(start);
-                  const end = videoSize - 1;
-
-            
-                  const contentLength = end - start + 1;
-                  const headers = {
-                    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-                    "Accept-Ranges": "bytes",
-                    "Content-Length": contentLength,
-                    "Content-Type": "video/mp4",
-                  };
-            
-                  // HTTP Status 206 for Partial Content
-                  res.writeHead(206, headers);
-            
-                  const bucket = new mongodb.GridFSBucket(db);
-                  const downloadStream = bucket.openDownloadStreamByName('bigbuck', {
-                    start
-                  });
-            
-                  // Finally pipe video to response
-                  downloadStream.pipe(res);
-                });
-            });
-        })
-
-        // Rutas para videos
-        app.route('/video')
-
-            // Obtener todos los videos
-            .get((req, res) => {
-                mongodb.MongoClient.connect(uri, function (error, client) {
-                    if (error) {
-                      res.status(500).json(error);
-                      return;
-                    }
-                
-                    const range = req.headers.range;
-                    if (!range) {
-                      res.status(400).send("Requires Range header");
-                    }
-                
-                    const db = client.db('Editors');
-                    // GridFS Collection
-                    db.collection('fs.files').find({}, (err, video) => {
-                      if (!video) {
                         res.status(404).send("No video uploaded!");
                         return;
-                      }
-                
-                      // Create response headers
-                      const videoSize = video.length;
-                      const start = Number(range.replace(/\D/g, ""));
-                      const end = videoSize - 1;
-                
-                      const contentLength = end - start + 1;
-                      const headers = {
+                    }
+
+                    // Create response headers
+                    const videoSize = video.length;
+                    const start = 0;
+                    console.log("START:");
+                    console.log(start);
+                    const end = videoSize - 1;
+
+
+                    const contentLength = end - start + 1;
+                    const headers = {
                         "Content-Range": `bytes ${start}-${end}/${videoSize}`,
                         "Accept-Ranges": "bytes",
                         "Content-Length": contentLength,
                         "Content-Type": "video/mp4",
-                      };
-                
-                      // HTTP Status 206 for Partial Content
-                      res.writeHead(206, headers);
-                
-                      const bucket = new mongodb.GridFSBucket(db);
-                      const downloadStream = bucket.openDownloadStreamByName('bigbuck', {
-                        start
-                      });
-                
-                      // Finally pipe video to response
-                      downloadStream.pipe(res);
-                    });
-                  });
-            })
+                    };
 
-            // Subir video
-            .post((req, res) => {
-                mongodb.MongoClient.connect(uri, function (error, client) {
-                    if (error) {
-                        res.json(error);
+                    // HTTP Status 206 for Partial Content
+                    res.writeHead(206, headers);
+
+                    const bucket = new mongodb.GridFSBucket(db);
+                    const downloadStream = bucket.openDownloadStreamByName('bigbuck', {
+                        start
+                    });
+
+                    // Finally pipe video to response
+                    downloadStream.pipe(res);
+                });
+            });
+        })
+
+    // Rutas para videos
+    app.route('/video')
+
+        // Obtener todos los videos
+        .get((req, res) => {
+            mongodb.MongoClient.connect(uri, function (error, client) {
+                if (error) {
+                    res.status(500).json(error);
+                    return;
+                }
+
+                const range = req.headers.range;
+                if (!range) {
+                    res.status(400).send("Requires Range header");
+                }
+
+                const db = client.db('Editors');
+                // GridFS Collection
+                db.collection('fs.files').find({}, (err, video) => {
+                    if (!video) {
+                        res.status(404).send("No video uploaded!");
                         return;
                     }
 
-                    const db = client.db('Editors');
+                    // Create response headers
+                    const videoSize = video.length;
+                    const start = Number(range.replace(/\D/g, ""));
+                    const end = videoSize - 1;
+
+                    const contentLength = end - start + 1;
+                    const headers = {
+                        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+                        "Accept-Ranges": "bytes",
+                        "Content-Length": contentLength,
+                        "Content-Type": "video/mp4",
+                    };
+
+                    // HTTP Status 206 for Partial Content
+                    res.writeHead(206, headers);
+
                     const bucket = new mongodb.GridFSBucket(db);
-                    const videoUploadStream = bucket.openUploadStream(req.file.originalname);
-                    const videoReadStream = fs.createReadStream('../frontend/public/uploads/' + req.file.originalname);
-                    videoReadStream.pipe(videoUploadStream);
-                    res.status(200).send("Done...");
-                  });
-            })
+                    const downloadStream = bucket.openDownloadStreamByName('bigbuck', {
+                        start
+                    });
+
+                    // Finally pipe video to response
+                    downloadStream.pipe(res);
+                });
+            });
+        })
+
+        // Subir video
+        .post((req, res) => {
+            mongodb.MongoClient.connect(uri, function (error, client) {
+                if (error) {
+                    res.json(error);
+                    return;
+                }
+
+                const db = client.db('Editors');
+                const bucket = new mongodb.GridFSBucket(db);
+                const videoUploadStream = bucket.openUploadStream(req.file.originalname);
+                const videoReadStream = fs.createReadStream('../frontend/public/uploads/' + req.file.originalname);
+                videoReadStream.pipe(videoUploadStream);
+                res.status(200).send("Done...");
+            });
+        })
 }
